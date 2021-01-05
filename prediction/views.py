b6d17cefc3dd636bbc8f1218d1a8e019fdb7ac2c
psajo <psajo@naver.com>
2021-01-05 18:43:03 +0900

    main페이지 초기버전 완성

--- prediction/views.py
+++ prediction/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Championdto
+from django.core import serializers
 # Create your views here.
 
 def index(request) :
-    champions = Championdto.objects.all()
+    rows = Championdto.objects.all()
+    champions = serializers.serialize('json', rows)
     context = {'champions':champions}
     return render(request,'prediction/main.html',context)
